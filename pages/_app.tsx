bc4eaff26a9fd73138314a66f0e4945a1685bcb2
PanagiotisC <panayiotischrysanthou56@gmail.com>
2025-07-19 15:34:18 +0300

    Add Vercel analytics and speed insights to the application, enhancing performance monitoring and user experience. Update package dependencies in package.json and package-lock.json to include new analytics packages.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp); 
